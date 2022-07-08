af568bde62a14cad5b8f39e8e3fafc34143238d2
LizMD <lizath09@gmail.com>
2022-07-08 00:14:08 +0000

    tarea modificada

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Footer () {
     return (
-<div className="card-footer p-5 m-4">
-    Copyright @ Your Website 2022
+<div class="card p-5 m-4">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">Copyright @ Your Website 2022</p>
+  </div>
 </div>
   );
 }
